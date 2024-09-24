b7fa9bc102e70c6cd448564af55b2f2cac81c147
Saleeshadenuwansl <nonesaliya@gmail.com>
2024-09-24 21:53:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "ke9TjRbR#vQwTBtUK5Esh_EWvvVzTWrTK7uOl16YaEQwTNQJ3bsE",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 MONGODB: process.env.MONGODB || "",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
